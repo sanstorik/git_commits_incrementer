7f3c1ab012016dc8c9e2f705582ff3d3b5a17071
shvetsovvitaliy06@gmail.com <sanstorik>
2019-07-22 18:02:12 +0300

    Moved github url to the env var

--- main.py
+++ main.py
@@ -6,10 +6,7 @@ import git
 import time
 
 
-while True:
-    current_date = dt.datetime.now()
-    readme_message = "Last commit - {0}\n".format(current_date)
-
+def run_git_script(commit_message, readme_message):
     bash_script = '''
     git config --global user.email {git_email}
     git config --global user.name {git_username}
@@ -18,9 +15,9 @@ while True:
     git remote set-url origin https://{git_username}:{git_password}@github.com/{git_username}/{repository}.git
     git pull origin master
     touch README.md
-    echo "{message}" >> README.md
+    echo "{r_message}" >> README.md
     git add "README.md"
-    git commit -a -m "Updated"
+    git commit -a -m {c_message}
     git push origin master
     cd --
     rm -r -d -f {repository}
@@ -28,11 +25,18 @@ while True:
         git_username = os.environ['GITHUB_USERNAME'],
         git_email = os.environ['GITHUB_EMAIL'],
         git_password = os.environ['GITHUB_PASSWORD'],
-        github_url = "https://github.com/sanstorik",
+        github_url = os.environ['GITHUB_URL'],
         repository = "git_commits_incrementer",
-        message = readme_message
+        c_message = commit_message,
+        r_message = readme_message
     )
 
     subprocess.run(bash_script, shell = True, check = True, executable = '/bin/bash')
 
+
+
+while True:
+    current_date = dt.datetime.now()
+    readme_message = "Last commit - {0}\n".format(current_date)
+    run_git_script("incremented", readme_message)
     time.sleep(10)
