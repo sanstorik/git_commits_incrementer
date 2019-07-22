ceff851197a0d3d4eb7b45bd2101da36c7de0ec4
shvetsovvitaliy06@gmail.com <sanstorik>
2019-07-22 18:18:24 +0300

    Try to clone again if was not successful.

--- main.py
+++ main.py
@@ -4,13 +4,19 @@ import subprocess
 import datetime as dt
 import time
 import random
+import uuid
 
 
 def run_git_script(commit_message, readme_message):
     bash_script = '''
     git config --global user.email {git_email}
     git config --global user.name {git_username}
-    git clone {github_url}/{repository}.git
+
+    DIR={repository}
+    if [ ! -d "$DIR" ]; then
+        git clone {github_url}/{repository}.git
+    fi
+
     cd {repository}
     git remote set-url origin https://{git_username}:{git_password}@github.com/{git_username}/{repository}.git
     git pull origin master
@@ -20,7 +26,6 @@ def run_git_script(commit_message, readme_message):
     git commit -a -m {c_message}
     git push origin master
     cd --
-    rm -r -d -f {repository}
     '''.format(
         git_username = os.environ['GITHUB_USERNAME'],
         git_email = os.environ['GITHUB_EMAIL'],
@@ -38,8 +43,13 @@ def run_git_script(commit_message, readme_message):
 while True:
     current_date = dt.datetime.now()
     readme_message = "Last commit - {0}\n".format(current_date)
-    run_git_script("incremented", readme_message)
+    run_git_script(uuid.uuid1(), readme_message)
  
     hour_in_seconds = 60 * 60
     sleep_time = random.randint(hour_in_seconds * 0.8, hour_in_seconds * 1.2)
+
+    DIR=os.environ['GITHUB_REPOSITORY']
+    if not os.path.exists('{dir}'.format(dir = DIR)):
+        sleep_time = 5
+
     time.sleep(sleep_time)
