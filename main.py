b0ef4acf6ff87b044fd0003169dcd408fb409a38
Developers <developer1@weinand.it>
2019-07-24 16:13:56 +0300

    e321f8cc-ae14-11e9-9634-9801a7a23557

--- main.py
+++ main.py
@@ -9,16 +9,12 @@ import uuid
 
 def run_git_script(commit_message, readme_message):
     bash_script = '''
-    git config --global user.email {git_email}
-    git config --global user.name {git_username}
-
-    DIR={repository}
+    DIR = {repository}
     if [ ! -d "$DIR" ]; then
-        git clone {github_url}/{repository}.git
+        git clone git@github.com:{git_username}/{repository}.git
     fi
 
     cd {repository}
-    git remote set-url origin https://{git_username}:{git_password}@github.com/{git_username}/{repository}.git
     git pull origin master
     touch README.md
     echo "{r_message}" > README.md
@@ -28,9 +24,6 @@ def run_git_script(commit_message, readme_message):
     cd --
     '''.format(
         git_username = os.environ['GITHUB_USERNAME'],
-        git_email = os.environ['GITHUB_EMAIL'],
-        git_password = os.environ['GITHUB_PASSWORD'],
-        github_url = os.environ['GITHUB_URL'],
         repository = os.environ['GITHUB_REPOSITORY'],
         c_message = commit_message,
         r_message = readme_message
@@ -41,13 +34,19 @@ def run_git_script(commit_message, readme_message):
 
 def script_sleep_interval(date):
     hour_in_seconds = 60 * 60
-    sleep_time = random.randint(hour_in_seconds * 0.8, hour_in_seconds * 1.3)
+    delay_min_multiplier = os.environ['MIN_MULTIPLIER']
+    delay_max_multiplier = os.environ['MAX_MULTIPLIER']
+
+    sleep_time = random.randint(
+        hour_in_seconds * delay_min_multiplier, 
+        hour_in_seconds * delay_max_multiplier
+    )
 
     is_working_day = current_date.weekday() < 5
     is_working_hour = current_date.hour >= 9 and current_date.hour <= 18
 
     if not (is_working_day and is_working_hour):
-        sleep_time *= 2.2
+        sleep_time *= 2
 
     return sleep_time
 
